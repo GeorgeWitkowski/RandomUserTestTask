a00ca6bd3c54e971ac82c9d5760f3b18b891755f
George Witkowski <vtkvsq@gmail.com>
2026-05-20 21:05:23 +0200

    Initial Commit

---
+++ RandomUserTestTask/RandomUserTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserTestTaskApp.swift
+//  RandomUserTestTask
+//
+//  Created by George on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
